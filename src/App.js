d3072823f629fa252c8b618ae717c50425709d3e
krokhale <krishna.rokhale@gmail.com>
2021-12-09 18:37:50 -0800

    checking in

--- src/App.js
+++ src/App.js
@@ -2,23 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    // const [categories, setCategory] = useState(['Category 1', 'Category 2']);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+        <div className={'border p-4 bg-gray-100'}>
+            <h1 className={'text-center text-xl uppercase tracking-wide'}>Question App</h1>
+        </div>
+
+        <div className={'w-full flex'}>
+
+            <div className={'w-1/4 h-screen bg-gray-100'}>
+                <ul>
+                    <li className={'p-8'}>
+                        <h1 className={'text-2xl uppercase tracking-widest'}>Category 1</h1>
+                    </li>
+                    <li className={'p-8 border'}>
+                        <h1 className={'text-2xl uppercase tracking-widest'}>Category 2</h1>
+                    </li>
+                    <li className={'p-8 bg-gray-300'}>
+                        <h1 className={'text-2xl uppercase tracking-widest'}>Category 3</h1>
+                    </li>
+                    <li className={'p-8 border'}>
+                        <h1 className={'text-2xl uppercase tracking-widest'}>Category 4</h1>
+                    </li>
+                    <li className={'p-8 border'}>
+                        <h1 className={'text-2xl uppercase tracking-widest'}>Category 5</h1>
+                    </li>
+                </ul>
+
+            </div>
+
+            <div className={'w-3/4 border h-screen bg-gray-200'}>
+
+            </div>
+
+        </div>
+
+    </>
   );
 }
 
